54febc778c52dbeda9adf125a732a7bbbe4480e0
ojimaluis <106823349+ojimaluis@users.noreply.github.com>
2023-06-20 15:03:11 +0200

    feat: add test ingredient.py

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,15 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    cheese = Ingredient("queijo provolone")
+    flour = Ingredient("farinha")
+    invalid = Ingredient("")
+
+    assert cheese.name == "queijo provolone"
+    assert cheese is not flour
+    assert cheese.restrictions
+    assert invalid.name == ""
+
+    assert cheese.__repr__() == "Ingredient('queijo provolone')"
+    assert cheese.__hash__() is cheese.__hash__()
+    assert cheese.__hash__() != flour.__hash__()
